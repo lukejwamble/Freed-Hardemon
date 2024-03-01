8c71d5264504ecda18909bcf5f720b8896028828
lukejwamble <wambleson3@gmail.com>
2024-03-01 16:34:23 -0600

    Update hardemons_class.py

--- hardemons_class.py
+++ hardemons_class.py
@@ -5,6 +5,7 @@ class Hardemon():
         self.attack = 0
         self.defense = 0
         self.speed = 0
+        self.moves = []
 
 davidshannon = Hardemon()
 davidshannon.name = "David Shannon"
@@ -12,6 +13,7 @@ davidshannon.health = 95
 davidshannon.attack = 95
 davidshannon.defense = 95
 davidshannon.speed = 115
+davidshannon.moves = ["Tackle"]
 
 tjkirk = Hardemon()
 tjkirk.name = "TJ Kirk"
@@ -19,6 +21,7 @@ tjkirk.health = 140
 tjkirk.attack = 60
 tjkirk.defense = 150
 tjkirk.speed = 50
+tjkirk.moves = ["Tackle"]
 
 lacycrowell = Hardemon()
 lacycrowell.name = "Lacy Crowell"
@@ -26,6 +29,7 @@ lacycrowell.health = 125
 lacycrowell.attack = 175
 lacycrowell.defense = 75
 lacycrowell.speed = 25
+lacycrowell.moves = ["Tackle"]
 
 kenancasey = Hardemon()
 kenancasey.name = "Kenan Casey"
@@ -33,6 +37,7 @@ kenancasey.health = 90
 kenancasey.attack = 120
 kenancasey.defense = 105
 kenancasey.speed = 85
+kenancasey.moves = ["Tackle"]
 
 lisaraine = Hardemon()
 kenancasey.name = "Lisa Raine"
@@ -40,6 +45,7 @@ kenancasey.health = 85
 kenancasey.attack = 120
 kenancasey.defense = 78
 kenancasey.speed = 117
+kenancasey.moves = ["Tackle"]
 
 jaredcollins = Hardemon()
 jaredcollins.name = "Jared Collins"
@@ -47,6 +53,7 @@ jaredcollins.health = 100
 jaredcollins.attack = 110
 jaredcollins.defense = 55
 jaredcollins.speed = 135
+jaredcollins.moves = ["Tackle"]
 
 davidtidwell = Hardemon()
 davidtidwell.name = "David Tidwell"
@@ -54,6 +61,7 @@ davidtidwell.health = 100
 davidtidwell.attack = 85
 davidtidwell.defense = 160
 davidtidwell.speed = 55
+davidtidwell.moves = ["Tackle"]
 
 dougburleson = Hardemon()
 dougburleson.name = "Doug Burleson"
@@ -61,6 +69,7 @@ dougburleson.health = 120
 dougburleson.attack = 75
 dougburleson.defense = 120
 dougburleson.speed = 75
+dougburleson.moves = ["Tackle"]
 
 donniedebord = Hardemon()
 donniedebord.name = "Donnie DeBord"
@@ -68,6 +77,7 @@ donniedebord.health = 75
 donniedebord.attack = 120
 donniedebord.defense = 75
 donniedebord.speed = 120
+donniedebord.moves = ["Tackle"]
 
 jimgardner = Hardemon()
 jimgardner.name = "Jim Gardner"
@@ -75,3 +85,9 @@ jimgardner.health = 25
 jimgardner.attack = 150
 jimgardner.defense = 25
 jimgardner.speed = 200
+jimgardner.moves = ["Tackle"]
+
+hardemon_list = [davidshannon, tjkirk, lacycrowell,
+                 kenancasey, lisaraine, jaredcollins, 
+                 davidtidwell, dougburleson, donniedebord, 
+                 jimgardner]
